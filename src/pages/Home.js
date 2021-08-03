3830bbeeb400e5f27fd142265fc701be2666a862
wahsandaruwan <wahsandaruwan6@gmail.com>
2021-08-03 18:20:42 +0530

    Home and Portfolio sample pages

---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <>
+            <h1>Himal Sandaruwan</h1>
+        </>
+    )
+}
+
+export default Home
---
+++ src/pages/Portfolio.js
@@ -0,0 +1,9 @@
+const Portfolio = () => {
+    return (
+        <>
+            <h1>My Portfolio</h1>
+        </>
+    )
+}
+
+export default Portfolio
