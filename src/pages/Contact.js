0553782ed5561cfae31c44833d0e864bc38e2141
wahsandaruwan <wahsandaruwan6@gmail.com>
2021-08-06 15:12:14 +0530

    Added a contact form button

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,7 @@
+// Componenets
 import Connect from "../components/Connect"
 import InputBox from "../components/InputBox"
+// Icons
 import {FaPhoneSquareAlt, FaMailBulk} from "react-icons/fa"
 
 const Contact = () => {
@@ -12,6 +14,7 @@ const Contact = () => {
                     <InputBox type="email" place="Enter Your Email"/>
                     <InputBox type="text" place="Enter Subject"/>
                     <textarea placeholder="Enter Your Message" className="txtar"></textarea>
+                    <button className="send-btn">Send Message</button>
                 </div>
                 <div className="con-box">
                     <Connect url="tel:0771005566" icon={<FaPhoneSquareAlt/>} text="+94-77-100-5566"/>
