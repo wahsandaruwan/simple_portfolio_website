998287658cdfe136f0b95f96781a71ada3812b4f
wahsandaruwan <wahsandaruwan6@gmail.com>
2021-08-14 16:30:31 +0530

    Error page and Portfolio page done

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,8 +1,15 @@
+// Components
+import {Link} from "react-router-dom"
+
 // Error Page Component
 const Error = () => {
     return (
         <>
-            <h1>Error 404 : Page Not Found</h1>  
+            <section className="error-page">
+                <h1>Error 404 : Page Not Found</h1>
+                <a href=""></a>
+                <Link className="back-btn" to="/">Back to Home</Link>
+            </section>
         </>
     )
 }
